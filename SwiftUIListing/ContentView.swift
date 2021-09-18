b08135a8e70e259ebcecbb09bebe1d91888f72df
Simranjeet <simranjeet@unyscape.com>
2021-09-18 16:33:05 +0530

    Initial Commit

---
+++ SwiftUIListing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIListing
+//
+//  Created by Desktop-simranjeet on 18/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
